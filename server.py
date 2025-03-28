f21fd9487c720006f75eebcce91799c50f153867
Luis Antonio Garcia <nem.luisg@coppel.com>
2025-03-28 14:43:12 -0600

    Fix cognitive complexity

--- server.py
+++ server.py
@@ -2,6 +2,39 @@ import socket
 from config import HOST, PORT
 
 
+def accept_connection(server_socket):
+    conn, addr = server_socket.accept()
+    print(f"Conexión establecida con {addr}")
+    handle_client(conn, addr)
+
+
+def handle_client(conn, addr):
+    try:
+        while True:
+            data = conn.recv(1024).decode()
+            if not data:
+                break
+            print(f"Mensaje recibido: {data}")
+
+            if data == "DESCONEXION":
+                print("Cliente desconectado")
+                break
+            else:
+                send_response(conn, data)
+    except ConnectionResetError:
+        print(f"Cliente {addr} desconectado..")
+    finally:
+        conn.close()
+
+
+def send_response(conn, data):
+    try:
+        conn.send(data.upper().encode())
+    except OSError:
+        print("Cliente desconectado")
+        conn.close()
+
+
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
@@ -9,30 +42,7 @@ def start_server():
     print(f"Servidor TCP escuchando en el puerto {PORT}...")
 
     while True:
-        conn, addr = server_socket.accept()
-        print(f"Conexión establecida con {addr}")
-
-        while True:
-            try:
-                data = conn.recv(1024).decode()
-            except ConnectionResetError:
-                print(f"Cliente {addr} desconectado..")
-                conn.close()
-            if not data:
-                break
-            print(f"Mensaje recibido: {data}")
-
-            if data == "DESCONEXION":
-                print("Cliente desconectado")
-                conn.close()
-                break
-            else:
-                try:
-                    conn.send(data.upper().encode())
-                except OSError:
-                    print("Cliente desconectado")
-                    conn.close()
-                    break
+        accept_connection(server_socket)
 
 
 def main():
